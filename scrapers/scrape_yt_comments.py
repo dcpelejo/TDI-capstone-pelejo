04cbb71a0adca20260d2e2e6b6b6d88ec3843ac2
dcpelejo <dcpelejo@gmail.com>
2022-11-20 18:09:36 -0500

    modified scraper to use local flat batabase file capstone.db and added sentiment_analysis.ipynb

--- scrapers/scrape_yt_comments.py
+++ scrapers/scrape_yt_comments.py
@@ -1,3 +1,4 @@
+import traceback
 import pandas as pd
 import dill
 
@@ -6,12 +7,12 @@ from dotenv import load_dotenv
 from apiclient.discovery import build
 from sqlalchemy import create_engine,text
 
-
 def create_connections():
     load_dotenv()
     key=os.getenv("GOOGLE_API_KEY")
     youtube = build("youtube", "v3", developerKey=key)
-    URL_DB = os.getenv("DATABASE_URL")
+    #URL_DB = os.getenv("DATABASE_URL")
+    URL_DB = 'sqlite:///capstone.db'
     db_engine = create_engine(URL_DB)
     conn=db_engine.connect()
     return youtube, conn
@@ -20,7 +21,7 @@ def create_connections():
 def vid_by_kw(keyword):
     """Searches for Youtube videos related to keyword. 
     Each page contains at most 50 results. """
-    npt=retrieve_NPT(keyword.replace(' ', '_'),'vid_by_kw')
+    npt=retrieve_NPT('vid_by_kw',keyword.replace(' ', '_'))
     try:
         result = youtube.search(
         ).list(q = keyword,
@@ -38,6 +39,8 @@ def vid_by_kw(keyword):
         return vids
     except Exception as e:
         print("The error raised is: ", e)
+        traceback_output = traceback.format_exc()
+        print(traceback_output)        
         return None
         
 def db_vid_by_kw(result,keyword):
@@ -53,23 +56,23 @@ def db_vid_by_kw(result,keyword):
            id=','.join(data['video_url'])
     ).execute()
     
-    data['num_views'] = [keyword['statistics']['viewCount'] for vid_url in response['items']]  
-    data['num_likes'] = [keyword['statistics']['likeCount'] for vid_url in response['items']]
-    data['num_comments'] = [keyword['statistics']['commentCount'] for vid_url in response['items']]
+    data['num_views'] = [vid_url['statistics'].get('viewCount',0) for vid_url in response['items']]  
+    data['num_likes'] = [vid_url['statistics'].get('likeCount',0) for vid_url in response['items']]
+    data['num_comments'] = [vid_url['statistics'].get('commentCount',0) for vid_url in response['items']]
 
     data.to_sql('youtube_videos',con=conn,if_exists='append',index=False)
         
-    return result['nextPageToken'], list(zip(data['video_url'], data['num_comments']))
+    return result.get('nextPageToken',None), list(zip(data['video_url'], data['num_comments']))
 
 def com_by_vid(video_url, num_comments):
     """Searches for comments on a given video url. 
     Each page contains at most 100 results. """
-    npt=retrieve_NPT(video_url,'com_by_vid')    
+    npt=retrieve_NPT('com_by_vid',video_url)    
     num=0
     while num<num_comments:
         try:
             result=youtube.commentThreads(
-            ).list(videoId='video_url',
+            ).list(videoId=video_url,
                    pageToken=npt,
                    part='snippet', 
                    maxResults=100,
@@ -81,6 +84,8 @@ def com_by_vid(video_url, num_comments):
             print(f"Video: {video_url}, Results: {num} of {num_comments} comments, nextPageToken: {npt}")
         except Exception as e:
             print("The error raised is: ",e)
+            traceback_output = traceback.format_exc()
+            print(traceback_output)   
             if num>0:
                 cache_NPT('com_by_vid',video_url,npt)
             break
@@ -96,16 +101,16 @@ def db_com_by_vid(result,video_url):
     data['video_url'] = [video_url for comment in result['items']]
     data.to_sql('youtube_comments',con=conn,if_exists='append',index=False)
     
-    return result['nextPageToken']
+    return result.get('nextPageToken',None)
 
 
-def cache_NPT(identifier, api_call, npt):
-    with open(api_call+identifier+'.pkd','wb') as cache:
+def cache_NPT(api_call,identifier, npt):
+    with open(api_call+'_'+identifier+'.pkd','wb') as cache:
         dill.dump(npt,cache)
         
-def retrieve_NPT(identifier,api_call):
+def retrieve_NPT(api_call,identifier):
     try:
-        with open(api_call+identifier+'.pkd','rb') as cache:
+        with open(api_call+'_'+identifier+'.pkd','rb') as cache:
             npt=dill.load(cache)
     except:
         npt=None 
@@ -119,11 +124,15 @@ if __name__=='__main__':
     youtube,conn=create_connections()
     while True:
         try:
+            print("Searching for videos related to:", keyword,"...") 
             vids=vid_by_kw(keyword)
             for i in vids:
+                print(f"Retrieving comments on video {i[0]} which has {i[1]} comments" )
                 com_by_vid(i[0],int(i[1]))
         except Exception as e:
             print('The error raised is:', e)
+            traceback_output = traceback.format_exc()
+            print(traceback_output)   
             break
     
     
